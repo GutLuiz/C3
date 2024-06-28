edb2c28644ac77356114e48ff712e1cac6c95fa3
Luiz Gustavo <gutfoda@gmail.com>
2024-06-28 13:01:42 -0300

    While

--- Program.cs
+++ Program.cs
@@ -4,48 +4,17 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        // Condicionais:
+        //While
 
-        // O else e if é mais usado para quando você quer fazer uma condicional apenas uma vez ou poucas vezes com uma variavel
-        Console.WriteLine("Digite um numero de 1 a 7: ");
-        int diaSemana = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite um numero positivo: ");
+        int valor = int.Parse(Console.ReadLine());
 
-        if (diaSemana == 1)
+        while (valor < 5)
         {
-            Console.WriteLine("Segunda");
-        }
-       else if (diaSemana == 2)
-        {
-            Console.WriteLine("terça");
-        }
-       else if (diaSemana == 3)
-        {
-            Console.WriteLine("quarta");
-        }
+            Console.WriteLine(  "Valor é: " + valor);
+            valor++;
 
-        // Quando a variavel vai ser utilizada varias vezes, como nesse exemplo, é mais organizado e didatico usar o switch case:
-
-        String nomeDiaSemana = "";
-
-        switch (diaSemana)
-        {
-            case 1:
-                nomeDiaSemana = "Segunda feira";
-                break;
-            case 2:
-                nomeDiaSemana = "Terça feira";
-                break;
-            case 3:
-                nomeDiaSemana = "Quarta feira";
-                break;
-            case 4:
-                nomeDiaSemana = "Quinta feira";
-                break;
-            case 5:
-                nomeDiaSemana = "Sexta feira";
-                break;
         }
-        Console.WriteLine(nomeDiaSemana);
     }
 
 }
