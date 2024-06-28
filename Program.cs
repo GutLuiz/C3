11023e04e43492a46dd6a8b3886b65f923d0e2c3
Luiz Gustavo <gutfoda@gmail.com>
2024-06-28 11:51:24 -0300

    String e convertendo um inteiro para string

--- Program.cs
+++ Program.cs
@@ -2,9 +2,14 @@
 {
     private static void Main(string[] args)
     {
+        // String 
         String nome = "santos";
         Console.WriteLine(nome);
 
-        
+        // Coventendo uma String
+        String nome1 = Convert.ToString("123");
+        Console.WriteLine(nome1);
+
+
     }
 }
