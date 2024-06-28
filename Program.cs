40093c0a6540582fa246f1d6427c3cae394a17c2
Luiz Gustavo <gutfoda@gmail.com>
2024-06-28 12:55:12 -0300

    Condicionais

--- Program.cs
+++ Program.cs
@@ -4,26 +4,48 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        bool valor = false;
-        bool valor2 = Convert.ToBoolean(true);
+        // Condicionais:
 
-        if (valor == true)
+        // O else e if é mais usado para quando você quer fazer uma condicional apenas uma vez ou poucas vezes com uma variavel
+        Console.WriteLine("Digite um numero de 1 a 7: ");
+        int diaSemana = int.Parse(Console.ReadLine());
+
+        if (diaSemana == 1)
         {
-            Console.WriteLine("Verdadeiro");
+            Console.WriteLine("Segunda");
         }
-        else
+       else if (diaSemana == 2)
         {
-            Console.WriteLine("Falso");
+            Console.WriteLine("terça");
+        }
+       else if (diaSemana == 3)
+        {
+            Console.WriteLine("quarta");
         }
 
-        if (valor2 == true)
+        // Quando a variavel vai ser utilizada varias vezes, como nesse exemplo, é mais organizado e didatico usar o switch case:
+
+        String nomeDiaSemana = "";
+
+        switch (diaSemana)
         {
-            Console.WriteLine("Verdadeiro");
+            case 1:
+                nomeDiaSemana = "Segunda feira";
+                break;
+            case 2:
+                nomeDiaSemana = "Terça feira";
+                break;
+            case 3:
+                nomeDiaSemana = "Quarta feira";
+                break;
+            case 4:
+                nomeDiaSemana = "Quinta feira";
+                break;
+            case 5:
+                nomeDiaSemana = "Sexta feira";
+                break;
         }
-        else
-        {
-            Console.WriteLine("Falso");
-        }
+        Console.WriteLine(nomeDiaSemana);
     }
 
 }
